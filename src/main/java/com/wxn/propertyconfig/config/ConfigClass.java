adefe935091612ef20daf82e62a9b05745a5fee3
wangxiaonan <wangxiaonan_0553@163.com>
2018-08-08 20:10:45 +0800

    fix

--- src/main/java/com/wxn/propertyconfig/config/ConfigClass.java
+++ src/main/java/com/wxn/propertyconfig/config/ConfigClass.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +22,17 @@ import java.util.List;
 @Configuration
 public class ConfigClass {
     @Bean
-    public PropertyPlaceholderConfigurer properties() {
+    public PropertyPlaceholderConfigurer properties() throws IOException {
         PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
         List<Resource> resources = new ArrayList<>();
-
         resources.add(new ClassPathResource("config/common.properties"));
-
+        // 批量添加resource
+//        try {
+//            Resource[] resource = resolver.getResources("classpath:**/*.yml");
+//            propertyPlaceholderConfigurer.setLocations(resource);
+//        } catch (IOException e) {
+//            logger.error("[PropertyConfig] initProperties fail, init file type *.yml", e);
+//        }
         ppc.setLocations(resources.toArray(new Resource[]{}));
         return ppc;
     }
